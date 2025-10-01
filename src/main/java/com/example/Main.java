51b700d4cc1e6147fcaccbd631665851449d6162
Eleni001 <Eleni.SolomonSamuelsson@medieinstitutet.se>
2025-10-01 11:59:21 +0200

    extract zone and date from command line and fetch the data

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -2,8 +2,38 @@ package com.example;
 
 import com.example.api.ElpriserAPI;
 
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         ElpriserAPI elpriserAPI = new ElpriserAPI();
+        String zone = "";
+        String date = "";
+
+        for (int i = 0; i < args.length; i++) {
+            System.out.println(args[i]);
+            if (args[i] .equals("--zone")) {
+                zone = args[i + 1];
+                i++;
+            } else if (args[i] .equals("--date")) {
+                date = args[i + 1];
+                i++;
+            }
+        }
+        System.out.println(zone);
+        System.out.println(date);
+        ElpriserAPI.Prisklass prisklass = parsePrisklass(zone);
+        List<ElpriserAPI.Elpris> dagensElPriser = elpriserAPI.getPriser(date, prisklass);
+        System.out.println(dagensElPriser);
+    }
+
+    private static ElpriserAPI.Prisklass parsePrisklass (String s) {
+        return switch (s){
+            case "SE1" -> ElpriserAPI.Prisklass.SE1;
+            case "SE2" -> ElpriserAPI.Prisklass.SE2;
+            case "SE3" -> ElpriserAPI.Prisklass.SE3;
+            case "SE4" -> ElpriserAPI.Prisklass.SE4;
+            default -> throw new RuntimeException("Unknown Prisklass " + s);
+        };
     }
 }
